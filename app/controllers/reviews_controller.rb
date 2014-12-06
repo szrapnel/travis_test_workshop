50618d79f03a0e1ae3eb7f2000e7446b6c03a9f2
konrad <konrad@debian>
2014-12-07 00:30:36 +0100

    9. Check if each review is assigned to user who wrote it.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
 
   expose(:review)
   expose(:product)
@@ -8,6 +9,7 @@ class ReviewsController < ApplicationController
 
   def create
     self.review = Review.new(review_params)
+    review.user=current_user
 
     if review.save
       product.reviews << review
