7a9703745daa080473d3397d784f4bf88967b5d1
konrad <konrad@debian>
2014-12-07 00:35:01 +0100

    10. If some actions (like links to edit a page, create a new one) are not allowed for a particular user then please hide them in a template (for example with `if`).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def admin_only_link_to(body, url, html_options={})
+    if current_user_is_admin?
+      link_to body, url, html_options
+    end
+  end
+
+  def current_user_is_admin?
+    if user_signed_in?
+      return current_user.admin
+    end
+  end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def owner_only_link_to(product, body, url, html_options={})
+    if user_signed_in?
+      if product.is_owned_by?(current_user)
+        link_to body, url, html_options
+      end
+    end
+  end
 end
