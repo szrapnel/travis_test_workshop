d54abd0eea1831afe91ee746567ed652d471af74
konrad <konrad@debian>
2014-12-07 00:21:28 +0100

    4. Next up: `Product` model and `spec/models/product_spec.rb`. Play with validations a bit, calculate average rating and you'll be good to go.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,21 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :price
+  validates_numericality_of :price
+  validates_format_of :price, :with => /\A\d+\.*\d{0,2}\Z/
+
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  def average_rating 
+    if reviews.empty?
+      return 0
+    else
+      sum=0
+      reviews.each do |r|
+        sum+=r.rating
+      end
+      return sum.to_f/reviews.count
+    end
+  end
 end
