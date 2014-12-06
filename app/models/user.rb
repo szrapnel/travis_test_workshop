9a379fb124e3f7cf41bf711869e78c9388740ac4
konrad <konrad@debian>
2014-12-06 23:07:14 +0100

    1. There are a few missing fields on the `User` model. Make sure `spec/models/user_spec.rb passes.`

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :firstname, :lastname
+
   has_many :reviews
   has_many :products
 end
