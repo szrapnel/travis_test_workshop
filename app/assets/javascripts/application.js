1af702e91f4d50b545861e400bd49dce7f4c13fb
konrad <konrad@debian>
2014-12-07 01:02:29 +0100

    clickable subitems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,17 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$( document ).ready(function() {
+    $(".subitem-hover").click(function() {
+  		item_click_redirect(this)
+  	})
+});
+
+function item_click_redirect(item) {
+	if(item.dataset['link']!=null) {
+		var address=item.dataset['link']
+		window.location=address;
+	}
+}
+
